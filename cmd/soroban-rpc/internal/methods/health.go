367a9367e7a5758c6eaef44d93a69747f44700b5
Alfonso Acosta <alfonso@stellar.org>
2024-04-12 02:37:31 +0200

    Add ledger range to getHealth endpoint (#133)

--- cmd/soroban-rpc/internal/methods/health.go
+++ cmd/soroban-rpc/internal/methods/health.go
@@ -8,24 +8,32 @@ import (
 	"github.com/creachadair/jrpc2"
 	"github.com/creachadair/jrpc2/handler"
 
-	"github.com/stellar/soroban-rpc/cmd/soroban-rpc/internal/transactions"
+	"github.com/stellar/soroban-rpc/cmd/soroban-rpc/internal/ledgerbucketwindow"
 )
 
 type HealthCheckResult struct {
-	Status string `json:"status"`
+	Status                string `json:"status"`
+	LatestLedger          uint32 `json:"latestLedger"`
+	OldestLedger          uint32 `json:"oldestLedger"`
+	LedgerRetentionWindow uint32 `json:"ledgerRetentionWindow"`
+}
+
+type LedgerRangeGetter interface {
+	GetLedgerRange() ledgerbucketwindow.LedgerRange
 }
 
 // NewHealthCheck returns a health check json rpc handler
-func NewHealthCheck(txStore *transactions.MemoryStore, maxHealthyLedgerLatency time.Duration) jrpc2.Handler {
+func NewHealthCheck(retentionWindow uint32, ledgerRangeGetter LedgerRangeGetter, maxHealthyLedgerLatency time.Duration) jrpc2.Handler {
 	return handler.New(func(ctx context.Context) (HealthCheckResult, error) {
-		ledgerInfo := txStore.GetLatestLedger()
-		if ledgerInfo.Sequence < 1 {
+		ledgerRange := ledgerRangeGetter.GetLedgerRange()
+		if ledgerRange.LastLedger.Sequence < 1 {
 			return HealthCheckResult{}, jrpc2.Error{
 				Code:    jrpc2.InternalError,
 				Message: "data stores are not initialized",
 			}
 		}
-		lastKnownLedgerCloseTime := time.Unix(ledgerInfo.CloseTime, 0)
+
+		lastKnownLedgerCloseTime := time.Unix(ledgerRange.LastLedger.CloseTime, 0)
 		lastKnownLedgerLatency := time.Since(lastKnownLedgerCloseTime)
 		if lastKnownLedgerLatency > maxHealthyLedgerLatency {
 			roundedLatency := lastKnownLedgerLatency.Round(time.Second)
@@ -35,6 +43,12 @@ func NewHealthCheck(txStore *transactions.MemoryStore, maxHealthyLedgerLatency t
 				Message: msg,
 			}
 		}
-		return HealthCheckResult{Status: "healthy"}, nil
+		result := HealthCheckResult{
+			Status:                "healthy",
+			LatestLedger:          ledgerRange.LastLedger.Sequence,
+			OldestLedger:          ledgerRange.FirstLedger.Sequence,
+			LedgerRetentionWindow: retentionWindow,
+		}
+		return result, nil
 	})
 }
