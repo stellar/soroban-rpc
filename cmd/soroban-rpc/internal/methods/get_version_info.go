a95ce3dde30c965548a8690e1c5ac8df928755d1
pritsheth <prit.sheth@stellar.org>
2024-04-12 11:46:07 -0700

    Add integration tests

--- cmd/soroban-rpc/internal/methods/get_version_info.go
+++ cmd/soroban-rpc/internal/methods/get_version_info.go
@@ -8,30 +8,29 @@ import (
 	"github.com/stellar/soroban-rpc/cmd/soroban-rpc/internal/daemon/interfaces"
 )
 
-type GetVersionRequest struct {
+type GetVersionInfoRequest struct {
 }
 
-type GetVersionResponse struct {
+type GetVersionInfoResponse struct {
 	Version                string `json:"version"`
 	CommitHash             string `json:"commit_hash"`
 	BuildTimestamp         string `json:"build_time_stamp"`
 	ProtocolVersion        int    `json:"protocol_version"`
-	XDRVersion             string `json:"xdr_version"`
 	CaptiveCoreVersionInfo string `json:"captive_core_version"`
 }
 
 func NewGetVersionInfoHandler(daemon interfaces.Daemon) jrpc2.Handler {
 	coreClient := daemon.CoreClient()
-	return handler.New(func(ctx context.Context, request GetVersionRequest) (GetVersionResponse, error) {
+	return handler.New(func(ctx context.Context, request GetVersionInfoRequest) (GetVersionInfoResponse, error) {
 		info, err := coreClient.Info(ctx)
 		if err != nil {
-			return GetVersionResponse{}, &jrpc2.Error{
+			return GetVersionInfoResponse{}, &jrpc2.Error{
 				Code:    jrpc2.InternalError,
 				Message: err.Error(),
 			}
 		}
 
-		return GetVersionResponse{
+		return GetVersionInfoResponse{
 			Version:                config.Version,
 			CommitHash:             config.CommitHash,
 			BuildTimestamp:         config.BuildTimestamp,
---
+++ cmd/soroban-rpc/internal/test/get_version_info_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/creachadair/jrpc2"
+	"github.com/creachadair/jrpc2/jhttp"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stellar/soroban-rpc/cmd/soroban-rpc/internal/methods"
+)
+
+func TestGetVersionInfoSucceeds(t *testing.T) {
+	test := NewTest(t, nil)
+
+	ch := jhttp.NewChannel(test.sorobanRPCURL(), nil)
+	client := jrpc2.NewClient(ch, nil)
+
+	request := methods.GetVersionInfoRequest{}
+
+	var result methods.GetVersionInfoResponse
+	err := client.CallResult(context.Background(), "getVersionInfo", request, &result)
+	assert.NoError(t, err)
+	assert.Equal(t, stellarCoreProtocolVersion, result.ProtocolVersion)
+}
