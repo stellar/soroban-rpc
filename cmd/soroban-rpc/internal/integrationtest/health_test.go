ef78ce0da6e39b333bc9b95d460005be68d8e62f
Alfonso Acosta <alfonso@stellar.org>
2024-06-27 16:19:15 +0200

    Unify transaction and event retention windows (#234)
    * Unify transaction and event retention windows
    
    Use a new, `unified history-retention-window` flag and mark the event/transaction window
    configuration parameters as deprecated.
    
    Until we remove the event/transaction flag, `history-retention-window` will be initialized to
    the maximum of the three.
    
    * Appease the linter

--- cmd/soroban-rpc/internal/integrationtest/health_test.go
+++ cmd/soroban-rpc/internal/integrationtest/health_test.go
@@ -6,8 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/stellar/soroban-rpc/cmd/soroban-rpc/internal/config"
 	"github.com/stellar/soroban-rpc/cmd/soroban-rpc/internal/integrationtest/infrastructure"
-	"github.com/stellar/soroban-rpc/cmd/soroban-rpc/internal/ledgerbucketwindow"
 )
 
 func TestHealth(t *testing.T) {
@@ -15,7 +15,7 @@ func TestHealth(t *testing.T) {
 	result, err := test.GetRPCHealth()
 	require.NoError(t, err)
 	assert.Equal(t, "healthy", result.Status)
-	assert.Equal(t, uint32(ledgerbucketwindow.OneDayOfLedgers), result.LedgerRetentionWindow)
+	assert.Equal(t, uint32(config.OneDayOfLedgers), result.LedgerRetentionWindow)
 	assert.Greater(t, result.OldestLedger, uint32(0))
 	assert.Greater(t, result.LatestLedger, uint32(0))
 	assert.GreaterOrEqual(t, result.LatestLedger, result.OldestLedger)
