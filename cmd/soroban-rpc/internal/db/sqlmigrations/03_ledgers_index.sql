b21593c7a96042e64fbb80e82346004ec949b815
Aditya Vyas <aditya.vyas@stellar.org>
2024-07-01 10:34:55 -0400

    Remove index migration

--- cmd/soroban-rpc/internal/db/sqlmigrations/03_ledgers_index.sql
+++
@@ -1,7 +0,0 @@
--- +migrate Up
-
--- creating an index in the ledgers table on the sequence number.
-CREATE INDEX index_lcm_sequence ON ledger_close_meta(sequence);
-
--- +migrate Down
-DROP INDEX index_lcm_sequence;
