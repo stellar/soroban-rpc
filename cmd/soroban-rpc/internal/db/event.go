506bc3036649508be15f28f5794a1e5f3f85ea2d
Prit Sheth <prit.sheth@stellar.org>
2024-08-21 15:42:00 -0700

    Fix lint pt1

--- cmd/soroban-rpc/internal/db/event.go
+++ cmd/soroban-rpc/internal/db/event.go
@@ -50,6 +50,7 @@ func NewEventReader(log *log.Entry, db db.SessionInterface, passphrase string) E
 	return &eventHandler{log: log, db: db, passphrase: passphrase}
 }
 
+//nolint:gocognit
 func (eventHandler *eventHandler) InsertEvents(lcm xdr.LedgerCloseMeta) error {
 	txCount := lcm.CountTransactions()
 
