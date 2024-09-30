28d5afb184294207666722bdd5226abdfba5c242
Aditya Vyas <aditya.vyas@stellar.org>
2024-09-30 10:31:49 -0400

    Use strconv.Itoa

--- cmd/soroban-rpc/internal/methods/get_ledgers.go
+++ cmd/soroban-rpc/internal/methods/get_ledgers.go
@@ -118,7 +118,7 @@ func (h ledgersHandler) getLedgers(ctx context.Context, request GetLedgersReques
 	if err != nil {
 		return GetLedgersResponse{}, err
 	}
-	cursor := fmt.Sprintf("%d", ledgers[len(ledgers)-1].Sequence)
+	cursor := strconv.Itoa(int(ledgers[len(ledgers)-1].Sequence))
 
 	return GetLedgersResponse{
 		Ledgers:               ledgers,
