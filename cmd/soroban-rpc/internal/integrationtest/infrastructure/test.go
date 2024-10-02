b6fd2ba885b16f473ebbbb8e69cb8e2e5ea8e654
Alfonso Acosta <alfonso@stellar.org>
2024-10-02 15:16:21 +0200

    Bump integration tests Max Supported protocol version

--- cmd/soroban-rpc/internal/integrationtest/infrastructure/test.go
+++ cmd/soroban-rpc/internal/integrationtest/infrastructure/test.go
@@ -35,7 +35,7 @@ import (
 
 const (
 	StandaloneNetworkPassphrase = "Standalone Network ; February 2017"
-	MaxSupportedProtocolVersion = 21
+	MaxSupportedProtocolVersion = 22
 	FriendbotURL                = "http://localhost:8000/friendbot"
 	// Needed when Core is run with ARTIFICIALLY_ACCELERATE_TIME_FOR_TESTING=true
 	checkpointFrequency               = 8
